9722b9bd22c4351cbf5403af241818f8f4b4b0de
Abdullah Mohammad Jaid <jaid2016k@gmail.com>
2025-12-30 00:59:18 +0800

    Fix missing dictionary values in add_item function
    Added missing values for 'Star rating', 'Total number of reviews', and 'Business location' keys in the dictionary returned by add_item. This ensures all expected data fields are properly populated.

--- scraper.py
+++ scraper.py
@@ -16,7 +16,7 @@ def add_item(name, category, full_address, phone_number, website_url, rating, re
         "Full address" : full_address
         "Phone number" : phone_number
         "Website URL" : website_url
-        "Star rating"
-        "Total number of reviews"
-        "Business location"
+        "Star rating" : rating
+        "Total number of reviews" : reviews
+        "Business location" : location
     })
